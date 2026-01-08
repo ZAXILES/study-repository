a1e484038c59ee2eeef492812bc68f9ff2cccea8
Axiles <Nikitazahar117@gmail.com>
2026-01-08 15:05:06 +0500

    main commit

--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"awesomeProject/feature1"
-	"awesomeProject/feature2"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Hello Git")
 	feature1.Feature1()
-	feature2.Feature2()
 
 }
